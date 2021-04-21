960afc250c466489227f98988be143f4a0186112
HimbeerserverDE <himbeerserverde@gmail.com>
2021-04-21 09:00:52 +0200

    Use TLS

--- tlstest.go
+++ tlstest.go
@@ -1,14 +1,89 @@
 package main
 
 import (
+	"bufio"
+	"crypto/tls"
 	"log"
 	"net"
 )
 
-const host = "127.0.0.1:20202"
+const host = "127.0.0.1:20203"
 
 func handleClient(conn net.Conn) {
-	log.Print("Server accepted client at ", conn.RemoteAddr().String())
+	defer conn.Close()
+
+	addr := conn.RemoteAddr().String()
+
+	log.Print("Client at ", addr, " connected")
+
+	r := bufio.NewReader(conn)
+	for {
+		msg, err := r.ReadString('\n')
+		if err != nil {
+			log.Print(err)
+			return
+		}
+
+		log.Print("Server received packet from ", addr, ": ", msg)
+
+		if _, err = conn.Write([]byte("Hello client\n")); err != nil {
+			log.Print(err)
+			return
+		}
+	}
+}
+
+func serve(ready chan struct{}) {
+	crt, err := tls.LoadX509KeyPair("server.crt", "server.key")
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	conf := &tls.Config{Certificates: []tls.Certificate{crt}}
+
+	l, err := tls.Listen("tcp", host, conf)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer l.Close()
+
+	log.Print("Server listening on ", host)
+
+	close(ready)
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go handleClient(conn)
+	}
+}
+
+func connect(ready chan struct{}) {
+	<-ready
+
+	conf := &tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	conn, err := tls.Dial("tcp", host, conf)
+	if err != nil {
+		log.Print(err)
+		return
+	}
+	defer conn.Close()
+
+	log.Print("Client connection successful")
+
+	if _, err = conn.Write([]byte("Hello server\n")); err != nil {
+		log.Print(err)
+		return
+	}
 
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
@@ -17,45 +92,12 @@ func handleClient(conn net.Conn) {
 	}
 	buf = buf[:n]
 
-	log.Print("Server received packet from ", conn.RemoteAddr().String(), ": ", string(buf))
+	log.Print("Client received packet: ", string(buf))
 }
 
 func main() {
-	log.Print("Starting server on ", host)
+	ready := make(chan struct{})
 
-	l, err := net.Listen("tcp", host)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer l.Close()
-
-	go func() {
-		for {
-			c, err := l.Accept()
-			if err != nil {
-				log.Print(err)
-				continue
-			}
-
-			go handleClient(c)
-		}
-	}()
-
-	log.Print("Server started successfully")
-	log.Print("Starting client")
-
-	conn, err := net.Dial("tcp", host)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	log.Print("Client connection successful")
-
-	if _, err = conn.Write([]byte("hello")); err != nil {
-		log.Print(err)
-	}
-
-	for {
-	}
+	go serve(ready)
+	connect(ready)
 }
